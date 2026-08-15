5eb1cd4980fc32d1e186ed8e1f8cc5e183c81ea4
Gia Bao Huynh <giabaohuynh@usf.edu>
2026-08-15 02:38:13 -0400

    Thang bao beo added

---
+++ if_statement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+char name[50] ="";
+printf("Enter your name: ");
+fgets(name, sizeof(name), stdin);
+name[strcspn(name,"\n")] = '\0';
+
+if(strlen(name) == 0){
+    printf("You did NOT enter your name");
+}else{
+        printf("Hello %s", name);
+    }
+}
+            
+    
